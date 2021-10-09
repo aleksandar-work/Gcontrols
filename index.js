fa0f6fe43c4ee975d9fcbd6afe6a69012fc364a0
elitedev001 <juhanyu3031@gmail.com>
2021-10-09 10:14:31 -0400

    turing all bugs

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const makeCommit = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
-    .subtract(1, "y")
-    .add(4, "d")
+    .subtract(3, "y")
+    .add(5, "d")
     .add(x, "w")   
     .add(y, "d")
     .format();
