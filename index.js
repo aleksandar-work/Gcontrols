b242dde39f0930f7108efedb9b4c00c50b087e54
elitedev001 <juhanyu3031@gmail.com>
2021-10-09 09:52:22 -0400

    fix a issue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const makeCommit = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 6);
   const DATE = moment()
-    .subtract(9, "y")
+    .subtract(8, "y")
     .add(5, "d")
     .add(x, "w")
     .add(y, "d")
@@ -28,4 +28,4 @@ const makeCommit = (n) => {
   });
 };
 
-makeCommit(100);
+makeCommit(160);
